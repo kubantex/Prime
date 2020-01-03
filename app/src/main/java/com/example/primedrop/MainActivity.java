ee71931cac7d68db55b9b5393f3d62075491c418
kubantex <33428141+kubantex@users.noreply.github.com>
2020-01-03 15:04:55 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/primedrop/MainActivity.java
+++ app/src/main/java/com/example/primedrop/MainActivity.java
@@ -103,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
             soundPlayer.playHitPierwSound();
         }
 
-        if (bananyPierwY > frameHeight){
+        if (bananyPierwY > frameHeight && bananyPierwCenterX!=bananyZlozX){
             bananyPierwY = - 100;
             bananyPierwX = (float) Math.floor(Math.random() * (frameWidth - bananyPierw.getWidth()));
         }
@@ -318,9 +318,9 @@ public class MainActivity extends AppCompatActivity {
                             zmianaPozycji();
                         }
                     });
+                }
             }
-        }
-    }, 0, 20);
+        }, 0, 20);
 
     }
 
@@ -334,22 +334,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void calculateAndSetNewCompositeNumber() {
-        bananZlozWartosc = 22; //Tutaj wyliczenie jakiejs wartosci
+
+        int[] composite = {4,6,8,9,10,12,14,15,16,18,20,21,22,24,25,26,27,28,30,32,33,34,35,36,38,39,
+                40,42,44,45,46,48,49,50,51,52,54,55,56,57,58,60,62,63,64,65,66,68,69,70,
+                72,74,75,76,77,78,80,81,82,84,85,86,87,88,90,91,92,93,94,95,96,98,99,100};
+        int x = new Random().nextInt(composite.length);
+        bananZlozWartosc = composite[x];
         bananyZloz.setText(String.valueOf(bananZlozWartosc));
+
     }
 
     private void calculateAndSetNewPrimeNumber() {
-        bananPierwWartosc = 11; //Tutaj wyliczenie wartości pierwszej jakiejś
+
+        int[] prime = {1,2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97};
+        int rnd = new Random().nextInt(prime.length);
+        bananPierwWartosc = prime[rnd];
         bananyPierw.setText(String.valueOf(bananPierwWartosc));
-    }
 }
 
 
 
-
-
-
-
-
-
+}
 
